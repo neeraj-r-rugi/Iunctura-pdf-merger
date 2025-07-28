c4e590fb8105887f300e82752ae87abef6b8e71f
Neeraj R Rugi <texonace123@gmail.com>
2025-07-28 18:02:01 +0530

    Added License to each file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-"""
-Developed by Neeraj R Rugi
-Under GNU GPL License 
-"""
+# Iunctura PDF Merger: A powerful command-line tool for merging and converting PDF files with flexible file handling and organization options. Tired of manually combining lecture slides or documents? Iunctura simplifies the process by offering seamless PDF merging, file conversion, and recursive directory processing. 
+# Copyright (C) 2024 NEERAJ R RUGI
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #system Imports
 import argparse
@@ -11,7 +23,7 @@ import shutil
 #Source files
 import src.process as util
 import src.files as files
-import src.conersion as converter
+import conversion as converter
 import src.errors as error
 
 def init_parser()->argparse.ArgumentParser:
--- src/conversion.py
+++ src/conversion.py
@@ -1,3 +1,18 @@
+# Iunctura PDF Merger: A powerful command-line tool for merging and converting PDF files with flexible file handling and organization options. Tired of manually combining lecture slides or documents? Iunctura simplifies the process by offering seamless PDF merging, file conversion, and recursive directory processing. 
+# Copyright (C) 2024 NEERAJ R RUGI
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import pypdf as pdf
 import os
 from . import process
